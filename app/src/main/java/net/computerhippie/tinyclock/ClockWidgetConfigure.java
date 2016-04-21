6d3aa9a6c9af28e28a7f2142f7e3a77c9e17963b
Artem Kotov <artem.kotov@firstlinesoftware.com>
2016-04-21 10:47:08 +0300

    configure widget

--- app/src/main/java/net/computerhippie/tinyclock/ClockWidgetConfigure.java
+++ app/src/main/java/net/computerhippie/tinyclock/ClockWidgetConfigure.java
@@ -1,12 +1,36 @@
 package net.computerhippie.tinyclock;
 
 import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClockWidgetConfigure extends Activity {
+    private int appWidgetId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.clock_widget_configure);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            appWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+    }
+
+    public void OkClick(View v){
+        setWidget();
+    }
+
+    public void setWidget(){
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        setResult(RESULT_OK, resultValue);
+        finish();
     }
 }
